7cf8943a9b7686d68a0d6a25219c2adfc64a7804
dudosyka <a.hatson22@gmail.com>
2023-12-04 20:11:22 +0300

    Postgres configuration added

--- jvm/home/startup.sh
+++ jvm/home/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-/home/wait-for-it.sh maria:3306 -s -t 60
+# Chose one of them:
+# /home/wait-for-it.sh maria:3306 -s -t 60
+# /home/wait-for-it.sh postgres:5432 -s -t 60
 java -jar -Xmx7g /opt/application/app.jar
